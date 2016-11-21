c52690d684382455c2dde2df405e5994114adef9
Mahshad Eslamifar <mahshad.sing@gmail.com>
2016-11-21 08:34:53 -0500

    Added browser-sync task into gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
 
 // JS hint task
-
 gulp.task('jshint', function() {
   gulp.src('server.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
+
+//browser-sync task
+gulp.task('browser-sync', function(){
+  browserSync.init({
+    server:true,
+    directory: true,
+    files: '*/*.html'
+  });
+});
+
+gulp.task('default', ['jshint', 'browser-sync'])
