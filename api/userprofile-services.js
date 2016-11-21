80a6f8bf2ccccfc07043cf754db13336d38c79ec
Mahshad Eslamifar <mahshad.sing@gmail.com>
2016-11-21 08:41:45 -0500

    Step4: Added CRUD operations into userprofile services as domain model and added them into service.js

--- api/userprofile-services.js
+++ api/userprofile-services.js
@@ -1,7 +1,30 @@
-module.exports = {
-  getInput : function(req, res){
-     var input = req.params.input;
-     var result = { 'data' : input };
-     return res.json(result);
- }
-}
+
+var exports = module.exports = {};
+var repo = require('../DAL/models/repository-factory.js');
+
+exports.getInput = function(req, res){
+       var input = req.params.input;
+       var result = { 'data' : input };
+       return res.json(result);
+   };
+
+
+exports.getUserProfile = function(req, res){
+    var userprofileRepo = repo.getRepository("user_profile");
+    userprofileRepo.get(res);
+};
+
+exports.getUserProfileById = function(req, res){
+    var userprofileRepo = repo.getRepository("user_profile");
+    userprofileRepo.getById(req, res);
+};
+
+exports.createUserProfile = function(req, res){
+    var userprofileRepo = repo.getRepository("user_profile");
+    userprofileRepo.create(req, res);
+};
+
+exports.updateUserProfile = function(req, res){
+  var userprofileRepo = repo.getRepository("user_profile");
+  userprofileRepo.update(req, res);
+};
--- public/js/services/service.js
+++ public/js/services/service.js
@@ -1,6 +1,16 @@
 angular.module('userprofileSvcModule',[]).factory('userprofileSvc', ['$resource', function($resource){
 return{
-  queryInput: $resource('http://localhost:8080/api/userprofile/:input', {}, { get : { method: 'GET', params: { input : ''}, verifyCache: true}})
+  queryInput: $resource('http://localhost:8080/api/input/:input', {},
+      { get : { method: 'GET', params: { input : ''}, verifyCache: true},
+        create : { method: 'POST', params: {}, verifyCache: true}
+      }),
+  queryUserProfile:
+    $resource('http://localhost:8080/api/userprofile/:userprofileId', {},
+      { get : { method: 'GET', params: { userprofileId : ''}, verifyCache: true},
+        create: {method: 'POST', params: {}, verifyCache: true},
+        update: {method: 'PUT', params: { userprofileId : ''}, verifyCache: true}
+      })
     }
   }
+
 ]);
